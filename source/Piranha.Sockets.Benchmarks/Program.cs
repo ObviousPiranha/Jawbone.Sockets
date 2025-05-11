68e4656e6fd8b4b29a09414fb28e93998afe7b2e
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2025-05-10 19:57:21 -0600

    Demote TimeSpan (#1)

---
+++ source/Piranha.Sockets.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace Piranha.Sockets.Benchmarks;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<TimeSpanBenchmark>();
+    }
+}
